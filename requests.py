ef3a8f842011e32934c7011d8949bc742c1018e1
nathan s <nrs1905@rit.edu>
2022-10-25 17:51:18 -0400

    added to make_requests, but waiting on catalogue still finished request_completed function

--- requests.py
+++ requests.py
@@ -14,10 +14,7 @@ def print_request(connection):
     con = connection
 
 
-def make_request(connection, requester, requestee, barcode):
-    con = connection
-    user = requester  # intended for use once catalogue is implamented
-    other = requestee  # intended for use once catalogue is implamented
+def make_request(con, requester, requestee, barcode, daterequired, returnbydate):
     cur = con.cursor()
     cur.execute('select shareable, shared from tools where barcode = %i', barcode)
     rows = cur.fetchall()
@@ -25,12 +22,21 @@ def make_request(connection, requester, requestee, barcode):
         return 'Tool not shareable'
     elif rows[1] == True:
         return 'Tool currently being shared'
-    cur.execute("update tools set shareable = false shared = true where barcode = %i", barcode)
+    cur.execute("update tools set shared = true where barcode = %i", barcode)
+    cur.execute("select max(rid) from requests")
+    rows = cur.fetchall()
+    rid = rows[0]
+    cur.execute("insert into requests (rid, barcode, daterequired, status, returnbydate, requester, requestee) values "
+                "(%i, %i, %s, 0 %s,%s, %s)", rid, barcode, daterequired, returnbydate, requester, requestee)
+    con.commit()
     # update catalogue once that's implamented
     cur.close()
     con.close()
     return 'Tool Successfully Shared'
 
 
-def request_completed(connection):
-    temp = False
+def request_completed(con, requester, requestee, barcode):
+    cur = con.cursor()
+    cur.execute("update tools set shared = false where barcode = %i", barcode)
+    cur.execute("update requests set status = 1 where barcode = %i", barcode)
+    cur.close()
